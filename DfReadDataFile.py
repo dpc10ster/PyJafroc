aed788bd2d92efa55023c77334c6781411cf0ef7
dpc10ster <dpc10ster@gmail.com>
2022-01-18 21:48:18 -0500

    Update DfReadDataFile.py

--- DfReadDataFile.py
+++ DfReadDataFile.py
@@ -7,6 +7,12 @@ from openpyxl import load_workbook
 warnings.simplefilter("ignore")
 
 
+def myExit(testStr, correctNames, msg):
+    check = all(item in testStr for item in correctNames)
+    if not check:
+        sys.exit(msg)
+
+
 def DfReadDataFile(filename):
     """
     Parameters
@@ -19,13 +25,12 @@ def DfReadDataFile(filename):
 
     """
 
-    wb = load_workbook(filename=filename)
-    sheetnames = wb.sheetnames
-    correctNames = ["NL", "LL", "TRUTH"]
-    check = all(item in sheetnames for item in correctNames)
-    if not check:
-        sys.exit("Excel workbook is missing at least one of NL, LL or "
-                 "TRUTH worksheets.")
+    wb = load_workbook(filename)
+    testStr = wb.sheetnames
+    correctNames=["NL","LL","TRUTH"]
+    msg = "Excel workbook is missing at least one of NL, LL or "
+    "TRUTH worksheets."
+    myExit(testStr,correctNames,msg)
 
     ws = wb['TRUTH']
     data = ws.values
@@ -33,11 +38,10 @@ def DfReadDataFile(filename):
 # Get the first line in file as a header line
     columns = next(data)[0:]
     correctNames = ['CaseID', 'LesionID', 'Weight']
-    check = all(item in columns for item in correctNames)
-    if not check:
-        sys.exit(("Excel workbook TRUTH sheet has missing or incorrect " 
-            "required column names. These are the correct names: "
-            " 'CaseID', 'LesionID', 'Weight'"))
+    msg = ("Excel workbook TRUTH sheet has missing or incorrect "
+        "required column names. These are the correct names: "
+        " 'CaseID', 'LesionID', 'Weight'")
+    myExit(columns,correctNames,msg)
 
 # Create a DataFrame based on the second and subsequent lines of data
     df = pd.DataFrame(data, columns=columns)
@@ -52,12 +56,17 @@ def DfReadDataFile(filename):
     weightCol = df["Weight"]
     L=len(caseIDCol)
     allCases = np.unique(np.array(df["CaseID"]))
-    normalCases = df.loc[df['LesionID'] == 0]
+    normalCases = df.loc[df['LesionID'] == 0]["CaseID"]
     K1=len(normalCases)
-    abnormalCases = df.loc[df['LesionID'] == 1]
+    abnormalCases = df.loc[df['LesionID'] == 1]["CaseID"]
     K2=len(abnormalCases)
     K = K1 + K2
     
+    x=pd.Series(df["CaseID"])
+    x=x.isin(abnormalCases)
+    x=pd.Series(df["CaseID"][x])
+    x=x.value_counts()
+    perCase=x.sort_index()
     
     
     return(df)
