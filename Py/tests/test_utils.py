0906ca8133005500b26cfa32e132062c65cec769
dpc10ster <dpc10ster@gmail.com>
2022-02-27 21:17:20 -0500

    learning more from python website
    https://docs.python.org/3/library/unittest.html
    
    python test_utils.py
    python -m unittest test_utils.py
    python -m unittest discover

--- Py/tests/test_utils.py
+++ Py/tests/test_utils.py
@@ -26,5 +26,19 @@ def test_say_hello(self):
     self.assertNotEqual(utils.say_hello("Chandan"), "Hi, Chandan")
     self.assertNotEqual(utils.say_hello("Hafeez"), "Hi, Hafeez")
 
+def test_upper(self):
+    self.assertEqual('foo'.upper(), 'FOO')
+
+def test_isupper(self):
+    self.assertTrue('FOO'.isupper())
+    self.assertFalse('Foo'.isupper())
+
+def test_split(self):
+    s = 'hello world'
+    self.assertEqual(s.split(), ['hello', 'world'])
+    # check that s.split fails when the separator is not a string
+    with self.assertRaises(TypeError):
+        s.split(2)
+
 if __name__ == '__main__':
     unittest.main()
