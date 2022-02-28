27625d833454b729cd7558f54ca0ecdb2e24502d
dpc10ster <dpc10ster@gmail.com>
2022-02-27 21:53:54 -0500

    reading...
    Now on:
    https://docs.python.org/3/library/unittest.html
    For later:
    https://docs.python.org/3/library/unittest.html#organizing-tests

--- Py/tests/test_utils.py
+++ Py/tests/test_utils.py
@@ -14,31 +14,31 @@ class TestUtils(unittest.TestCase):
         self.assertEqual(utils.is_prime(-3),
                          "Negative numbers are not allowed")
                          
-def test_cubic(self):
-        self.assertEqual(utils.cubic(2), 8)
-        self.assertEqual(utils.cubic(-2), -8)
-        self.assertNotEqual(utils.cubic(2), 4)
-        self.assertNotEqual(utils.cubic(-3), 27)
-
-def test_say_hello(self):
-    self.assertEqual(utils.say_hello("Geekflare"), "Hello, Geekflare")
-    self.assertEqual(utils.say_hello("Chandan"), "Hello, Chandan")
-    self.assertNotEqual(utils.say_hello("Chandan"), "Hi, Chandan")
-    self.assertNotEqual(utils.say_hello("Hafeez"), "Hi, Hafeez")
-
-def test_upper(self):
-    self.assertEqual('foo'.upper(), 'FOO')
-
-def test_isupper(self):
-    self.assertTrue('FOO'.isupper())
-    self.assertFalse('Foo'.isupper())
-
-def test_split(self):
-    s = 'hello world'
-    self.assertEqual(s.split(), ['hello', 'world'])
-    # check that s.split fails when the separator is not a string
-    with self.assertRaises(TypeError):
-        s.split(2)
+    def test_cubic(self):
+            self.assertEqual(utils.cubic(2), 8)
+            self.assertEqual(utils.cubic(-2), -8)
+            self.assertNotEqual(utils.cubic(2), 4)
+            self.assertNotEqual(utils.cubic(-3), 27)
+    
+    def test_say_hello(self):
+        self.assertEqual(utils.say_hello("Geekflare"), "Hello, Geekflare")
+        self.assertEqual(utils.say_hello("Chandan"), "Hello, Chandan")
+        self.assertNotEqual(utils.say_hello("Chandan"), "Hi, Chandan")
+        self.assertNotEqual(utils.say_hello("Hafeez"), "Hi, Hafeez")
+    
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+    
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+    
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
 
 if __name__ == '__main__':
     unittest.main()
